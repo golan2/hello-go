02e1b09ee7be88d66165c5f5d5772a13253ba0e6
izik.golan <izik.golan@imubit.com>
2025-01-26 10:10:01 +0200

    interview: play_with_random.go

---
+++ pkg/maths/randi/play_with_random.go
@@ -0,0 +1,19 @@
+package randi
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func PlayWithRandom() {
+	arr := make([]int, 10)
+	for i := 0; i < 10; i++ {
+		arr[i] = i
+	}
+	r := rand.New(rand.NewSource(0))
+	r.Shuffle(10, func(i, j int) {
+		arr[i], arr[j] = arr[j], arr[i]
+	})
+
+	fmt.Printf("%v\n", arr)
+}
